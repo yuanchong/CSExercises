56e08d23f1361948389c630129990ec63fec56fc
Hans <yunghans@hotmail.com>
2015-08-06 18:41:38 +0800

    Edit ScoreMe.cmd

--- ScoreMe.cmd
+++ ScoreMe.cmd
@@ -1,6 +1,5 @@
 @echo Testing your project
 @pushd .
-@cd CSExercises\bin\debug
+@cd ExercisesTest\bin\debug
 @"%VS120COMNTOOLS%\..\ide\commonextensions\microsoft\testwindow\vstest.console.exe" ..\..\..\ExercisesTest.dll 
 @popd
-@type .git\FETCH_HEAD
